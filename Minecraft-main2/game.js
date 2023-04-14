687b908e8fa4794f93210e65399e4c6ee9d85f92
Matan-Shaked <118538183+Matan-Shaked@users.noreply.github.com>
2023-04-14 17:50:06 +0300

    Add files via upload

---
+++ Minecraft-main2/game.js
@@ -0,0 +1,6 @@
+// let div = document.querySelector(".containerGame");
+// for (let i = 1; i <= 324; i++) {
+//   let child = document.createElement("div");
+//   child.className = "child";
+//   div.append(child);
+// } grid with lotrs of children!!!
